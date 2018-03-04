d1b5b45e1dafbe4e8c1bbff9c8ae53fc16bb8210
Delton Myalil Antony <deltonguns@gmail.com>
2018-03-04 22:51:52 +0530

    Max and Min limits

--- README.txt
+++ README.txt
@@ -1,2 +1,2 @@
 1 - Hello
-2 - MaxRange: Derives the max range of int and the range in terms of 2's complement binary notation of 4Bytes
+2 - MaxRange: Derives the max range of int and the range in terms of 2's complement binary notation of 4Bytes. Also for IEEE 754 FP 64 and 32 bits
--- src/MaxRange.java
+++ src/MaxRange.java
@@ -14,5 +14,23 @@ public class MaxRange {
         long longMinValue= -9_223_372_036_854_775_808L; //Always put an 'L' or an 'l' at the end of the long value
         long longMaxValue= -9_223_372_036_854_775_807L; //Put L in the end
         //long is 64 bit 2's complement number, range is from -(2^64) to 2^64-1
+
+        //float and double
+        int myIntValue = 5;
+        float myFloatValue = 5f; //if you want to specifically instruct for float, append f to the value
+        //float takes up 32 bits IEEE 754 32 bit Standard. 1 bit for sign, 8 bits for biased exponent, 23 bits for Mantissa
+        double myDoubleValue = 5d;  //if you want to specifically instruct for double, append d to the value
+        //double takes up 64 bits of data. It is IEEE 754 64 bit Standard: 1 bit for sign, 11 bits for biased exponent, 20 bits for Mantissa
+        System.out.println("Integer Value is " + myIntValue/3);
+        System.out.println("Float Value is " + myFloatValue/3); // shows 7 decimal places
+        System.out.println("Double Value is " + myDoubleValue/3); // shows 16 decimal places
+        //Double is faster in 64 bit modern computers as they are optimised for 64bit FP computation
+        //Use double instead of float to make codes efficient
+
+        //char and boolean
+        char myChar = 'a'; // takes up two bytes
+        char unicodeChar = '\u0D05'; // malayalam letter aa - use \u followed by the code
+        System.out.println(unicodeChar);
+        boolean myBool = true; // unlike Python, t is not capitalized
     }
 }
