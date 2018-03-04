5ef319c959ce86d086be5b2e23ed115a286cc4bc
Delton Myalil Antony <deltonguns@gmail.com>
2018-03-04 21:19:08 +0530

    Max and Min limits

--- src/MaxRange.java
+++ src/MaxRange.java
@@ -7,5 +7,12 @@ public class MaxRange {
         byte byteMinValue= -128; // smallest value stored by a byte
         byte byteMaxValue= 127; // largest value stored by a byte is 127
         //byte uses 2's complement 8 bit binary representation: (-2)^(8-1)<=n<=2^(8-1)-1
+        //use byte if you want to store a small number
+        short shortMinValue= -32768; //short is 16 bit 2's complement
+        short shortMaxValue= 32767; // range of short is from -32k to 32k-1
+        // use short if you want to store a larger number than byte as short is made of two bytes
+        long longMinValue= -9_223_372_036_854_775_808L; //Always put an 'L' or an 'l' at the end of the long value
+        long longMaxValue= -9_223_372_036_854_775_807L; //Put L in the end
+        //long is 64 bit 2's complement number, range is from -(2^64) to 2^64-1
     }
 }
